64dc0467e9d7e4a19e9b1d8d18cee58463c61e2a
Darshan Shah <dpshah2895@gmail.com>
2020-04-25 01:17:38 -0400

    Updates

---
+++ precache-manifest.6a35c4c3f043ec2b0032c9958cacc1c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4855f56ffacb987d368c609fc691067",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8a7c859f92b8842dfeb4",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fb22ecad914df91789ec",
+    "url": "/portfolio/static/js/2.be534236.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.be534236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7c859f92b8842dfeb4",
+    "url": "/portfolio/static/js/main.10b0b3b9.chunk.js"
+  },
+  {
+    "revision": "12aed0e9fe497358bb01",
+    "url": "/portfolio/static/js/runtime-main.2c79e6b2.js"
+  }
+]);
