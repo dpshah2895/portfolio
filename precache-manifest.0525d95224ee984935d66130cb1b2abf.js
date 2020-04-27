0bf3b515a0ecc234dd43e78861ca8ba60cf3024a
Darshan Shah <dpshah2895@gmail.com>
2020-04-27 18:31:42 -0400

    Updates

---
+++ precache-manifest.0525d95224ee984935d66130cb1b2abf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67e0286623ec6d97ebebcbc0c40176f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "637f6a06f48dc3a8781e",
+    "url": "/portfolio/static/css/2.2284cef2.chunk.css"
+  },
+  {
+    "revision": "d20fa71046b1397ee5cb",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "637f6a06f48dc3a8781e",
+    "url": "/portfolio/static/js/2.16acfc9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.16acfc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d20fa71046b1397ee5cb",
+    "url": "/portfolio/static/js/main.e97cf63d.chunk.js"
+  },
+  {
+    "revision": "12aed0e9fe497358bb01",
+    "url": "/portfolio/static/js/runtime-main.2c79e6b2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1e1a4a35e95216fa5c175199ff48d7f0.js"
+  "/portfolio/precache-manifest.0525d95224ee984935d66130cb1b2abf.js"
 );
 
 self.addEventListener('message', (event) => {
