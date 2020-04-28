b4b7a6b165b67a6cf3b9f0752ce601d33a68727f
Darshan Shah <dpshah2895@gmail.com>
2020-04-27 20:15:46 -0400

    Updates

--- precache-manifest.a78f9bb87cd2d446b873f77c8540e1c3.js
+++ precache-manifest.a78f9bb87cd2d446b873f77c8540e1c3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "37af7ef0cfdece6db76230058e329fc1",
+    "revision": "f808b9d39e6eb5de5108011bba5dff85",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "833b45846623a0c25cff",
+    "revision": "80593a40934509986751",
     "url": "/portfolio/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.be534236.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "833b45846623a0c25cff",
-    "url": "/portfolio/static/js/main.da24d972.chunk.js"
+    "revision": "80593a40934509986751",
+    "url": "/portfolio/static/js/main.56f511a7.chunk.js"
   },
   {
     "revision": "12aed0e9fe497358bb01",
